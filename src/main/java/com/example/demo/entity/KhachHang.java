ffe24c6507074ac43dcc73a24c71d25fd35e392f
Perora <118367807+Pecora-pon@users.noreply.github.com>
2023-11-02 23:18:35 +0700

    Add files via upload

--- src/main/java/com/example/demo/entity/KhachHang.java
+++ src/main/java/com/example/demo/entity/KhachHang.java
@@ -1,7 +1,5 @@
 package com.example.demo.entity;
 
-import com.example.demo.entity.ChucVu;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,14 +7,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 
 @Entity
@@ -50,9 +45,7 @@ public class KhachHang {
     @Column(name = "SoDienThoai")
     private String soDienThoai;
     
-    @ManyToOne
-    @JoinColumn(name = "ChucVuID")
-    private ChucVu chucVu;
+
     
 
 
