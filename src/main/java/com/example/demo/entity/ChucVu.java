ab9e2bc95010e0d02552d68d114b54fcd7bae0d7
Perora <118367807+Pecora-pon@users.noreply.github.com>
2023-11-02 22:37:17 +0700

    Add files via upload

--- src/main/java/com/example/demo/entity/ChucVu.java
+++ src/main/java/com/example/demo/entity/ChucVu.java
@@ -7,32 +7,28 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
-@Table(name = "ThongTinVanChuyen")
-public class ThongTinVanChuyen {
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ChucVu")
+public class ChucVu {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ThongTinVanChuyenID")
-    private Integer thongTinVanChuyenID;
+    @Column(name = "ChucVuID")
+    private Integer chucVuID;
 
-    @Column(name = "DiaChi", columnDefinition = "NVARCHAR(MAX)")
-    private String diaChi;
-
-    @Column(name = "PhuongThuc", length = 255)
-    private String phuongThuc;
-
-    @Column(name = "TrangThai")
-    private Integer trangThai;
+    @Column(name = "TenChucVu")
+    @NotBlank(message = "không được trống")
+    private String tenChucVu;
 
 
 }
--- src/main/java/com/example/demo/entity/KhachHang.java
+++ src/main/java/com/example/demo/entity/KhachHang.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import com.example.demo.entity.ChucVu;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,11 +9,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 
 @Entity
@@ -28,13 +33,13 @@ public class KhachHang {
     private Integer khachHangId;
 
     @Column(name = "TenDangNhap")
-    private String tenDangNhap;
+    private String username;
 
     @Column(name = "Email")
     private String email;
 
     @Column(name = "MatKhau")
-    private String matKhau;
+    private String password;
 
     @Column(name = "HoTen")
     private String hoTen;
@@ -45,9 +50,11 @@ public class KhachHang {
     @Column(name = "SoDienThoai")
     private String soDienThoai;
     
-//    @ManyToOne
-//    @JoinColumn(name = "ChucVuID")
-//    private ChucVu chucVu;
+    @ManyToOne
+    @JoinColumn(name = "ChucVuID")
+    private ChucVu chucVu;
+    
+
 
 
 }
