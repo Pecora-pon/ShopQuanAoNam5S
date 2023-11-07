74813b96a460c3c3476d05da89dfb185db8f9730
Perora <118367807+Pecora-pon@users.noreply.github.com>
2023-11-07 13:17:11 +0700

    Add files via upload

--- src/main/java/com/example/demo/service/ShopService.java
+++ src/main/java/com/example/demo/service/ShopService.java
@@ -6,9 +6,19 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 
 public interface ShopService {
-    Page<SanPham> getPage(int pageNumber,int pageSize);
+    Page<SanPham> getPage(int pageNumber, int pageSize);
 
     List<SanPham> findByThuongHieu(String thuonghieuid);
+
+    List<SanPham> findByMauSacID(String mauSacID);
+
+    List<SanPham> findByChatLieuID(String chatLieuID);
+
+    List<SanPham> findBySizeID(String sizeID);
+
+    List<SanPham> findByTenSanPham(String tenSanPham);
+
+    List<SanPham> findByProductInPriceRange(Double minPrice,Double maxPrice);
 
     List<SanPham> getAll();
 
