6eda5b26cac709b04aa6917afb15122aa053b7ec
Perora <118367807+Pecora-pon@users.noreply.github.com>
2023-11-07 13:16:04 +0700

    Add files via upload

--- src/main/java/com/example/demo/repository/SanPhamRepo.java
+++ src/main/java/com/example/demo/repository/SanPhamRepo.java
@@ -13,6 +13,16 @@ import java.util.UUID;
 public interface SanPhamRepo extends JpaRepository<SanPham, UUID> {
     @Query("SELECT sp FROM SanPham sp WHERE sp.thuongHieu LIKE %?1%")
     List<SanPham> findByThuongHieuID(String thuongHieuID);
+    @Query("SELECT sp FROM SanPham sp WHERE sp.mauSac LIKE %?1%")
+    List<SanPham> findByMauSacID(String mauSacID);
+    @Query("SELECT sp FROM SanPham sp WHERE sp.chatLieu LIKE %?1%")
+    List<SanPham> findByChatLieuID(String chatLieuID);
+    @Query("SELECT sp FROM SanPham sp WHERE sp.size LIKE %?1%")
+    List<SanPham> findBySizeID(String sizeID);
+    @Query("SELECT sp FROM SanPham sp WHERE sp.tenSanPham LIKE %?1%")
+    List<SanPham> findByTenSanPham(String tenSanPham);
+    @Query("SELECT sp FROM SanPham sp WHERE sp.giaSanPham BETWEEN ?1 AND ?2")
+    List<SanPham> findProductsInPriceRange(Double minPrice,Double maxPrice);
 
 
     Optional<SanPham> findById(UUID sanPhamID);
