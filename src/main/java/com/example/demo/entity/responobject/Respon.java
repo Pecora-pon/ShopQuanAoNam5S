3465d232d78c02a9dc516bee97492aebc3ddc966
longthl <117787173+longthl@users.noreply.github.com>
2023-11-07 22:19:14 +0700

    Add files via upload

---
+++ src/main/java/com/example/demo/entity/responobject/Respon.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity.responobject;
+
+public class Respon<T> {
+    private String status;
+    private String error;
+    private T data;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
