6362735a40aa1b0c7c526c84c52d65577bddd5e5
Perora <118367807+Pecora-pon@users.noreply.github.com>
2023-12-06 17:52:21 +0700

    Add files via upload

---
+++ src/main/java/com/example/demo/service/ThongKeService.java
@@ -0,0 +1,71 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.DonHangChiTiet;
+import com.example.demo.entity.KhachHang;
+import com.example.demo.entity.NhanVien;
+import com.example.demo.entity.SanPham;
+import com.example.demo.repository.DonHangChiTietRepo;
+import com.example.demo.repository.KhachHangRepo;
+import com.example.demo.repository.NhanVienRepo;
+import com.example.demo.repository.SanPhamRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ThongKeService {
+    @Autowired
+    private NhanVienRepo nhanVienRepository;
+    @Autowired
+    private SanPhamRepo sanPhamRepo;
+    @Autowired
+    private KhachHangRepo khachHangRepo;
+    @Autowired
+    private DonHangChiTietRepo donHangChiTietRepo;
+
+    public List<NhanVien> getAllNhanVien() {
+        return nhanVienRepository.findAll();
+    }
+
+    public long countNhanVien() {
+        return nhanVienRepository.count();
+    }
+
+    public List<SanPham> getAllSanPham() {
+        return sanPhamRepo.findAll();
+    }
+
+    public long countSanPham() {
+        return sanPhamRepo.count();
+    }
+    public List<KhachHang> getAllKhachHang() {
+        return khachHangRepo.findAll();
+    }
+
+    public long countKhachHang() {
+        return khachHangRepo.count();
+    }
+    public List<DonHangChiTiet> getAllDonHangChiTiet() {
+        return donHangChiTietRepo.findAll();
+    }
+
+    public double calculateTotalTongTien(int trangThai) {
+        List<DonHangChiTiet> donHangChiTiets = donHangChiTietRepo.findByTrangThai(trangThai);
+        double totalTongTien = 0;
+
+        for (DonHangChiTiet donHangChiTiet : donHangChiTiets) {
+            totalTongTien += donHangChiTiet.getTongTien();
+        }
+
+        return totalTongTien;
+    }
+    public List<Object[]> getTotalQuantityByMonthInYear2023() {
+        return donHangChiTietRepo.getTotalQuantityByMonthInYear2023();
+    }
+    public List<Object[]> getTotalRevenueByMonthInYear2023() {
+        return donHangChiTietRepo.getTotalRevenueByMonthInYear2023();
+    }
+
+
+}
