639fbdb2b24a4ceae966222084f5ccedb69fd8e5
Perora <118367807+Pecora-pon@users.noreply.github.com>
2023-12-06 17:51:55 +0700

    Add files via upload

--- src/main/java/com/example/demo/repository/DonHangChiTietRepo.java
+++ src/main/java/com/example/demo/repository/DonHangChiTietRepo.java
@@ -10,23 +10,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 @Repository
 public interface DonHangChiTietRepo extends JpaRepository<DonHangChiTiet, Integer> {
-    @Query("select p from DonHangChiTiet p WHERE p.donHang.khachHang.username =:username and p.trangThai =0 order by p.donHangChiTietID desc")
-    List<DonHangChiTiet>findByDonHang_KhachHang_Username(String username);
-    @Query("select p from DonHangChiTiet p WHERE p.donHang.khachHang.username =:username and p.trangThai =1 order by p.donHangChiTietID desc")
-    List<DonHangChiTiet>findByDonHang_KhachHang_Username1(String username);
-    @Query("select p from DonHangChiTiet p WHERE p.donHang.khachHang.username =:username and p.trangThai =2 order by p.donHangChiTietID desc")
-    List<DonHangChiTiet>findByDonHang_KhachHang_Username2(String username);
-    @Query("select p from DonHangChiTiet p WHERE p.donHang.khachHang.username =:username and p.trangThai =3 order by p.donHangChiTietID desc")
-    List<DonHangChiTiet>findByDonHang_KhachHang_Username3(String username);
-    @Query("select p from DonHangChiTiet p WHERE p.donHang.khachHang.username =:username and p.trangThai =4 order by p.donHangChiTietID desc")
-    List<DonHangChiTiet>findByDonHang_KhachHang_Username4(String username);
-    DonHangChiTiet findByDonHang_NgayDatHang(LocalDate ngaydathang);
-    List<DonHangChiTiet> findByDonHang_DonHangID(UUID id);
     @Transactional
     @Modifying
     @Query(value = "Update DonHangChiTiet set trangThai = 1 where donHangID =:donHangID",nativeQuery = true)
@@ -55,4 +42,10 @@ public interface DonHangChiTietRepo extends JpaRepository<DonHangChiTiet, Intege
     void chuyensanghuy(@Param("donHangID") UUID donHangChiTietID);
     @Query(value = "select * from DonHangChiTiet p WHERE p.trangThai =4 order by p.donHangChiTietID desc",nativeQuery = true)
     List<DonHangChiTiet> getHuyy();
+    @Query("SELECT EXTRACT(MONTH FROM dh.ngayDatHang) AS month, SUM(dhc.soLuong) AS totalQuantity FROM DonHangChiTiet dhc JOIN dhc.donHang dh WHERE dh.trangThai = 3 AND EXTRACT(YEAR FROM dh.ngayDatHang) = 2023 GROUP BY EXTRACT(MONTH FROM dh.ngayDatHang)")
+    List<Object[]> getTotalQuantityByMonthInYear2023();
+    @Query("SELECT MONTH(dhc.donHang.ngayDatHang) AS month, SUM(dhc.tongTien) AS totalRevenue FROM DonHangChiTiet dhc WHERE dhc.donHang.trangThai = 3 AND EXTRACT(YEAR FROM dhc.donHang.ngayDatHang) = 2023 GROUP BY MONTH(dhc.donHang.ngayDatHang)")
+    List<Object[]> getTotalRevenueByMonthInYear2023();
+    List<DonHangChiTiet> findByTrangThai(int trangThai);
+
 }
