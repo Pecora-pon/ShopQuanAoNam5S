2f02f5c21307375915194cdf4832568f60c1bb29
longthl <117787173+longthl@users.noreply.github.com>
2023-12-07 23:13:41 +0700

    Add files via upload

--- src/main/java/com/example/demo/entity/DonHangChiTiet.java
+++ src/main/java/com/example/demo/entity/DonHangChiTiet.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity
@@ -49,6 +50,7 @@ public class DonHangChiTiet {
     @Column(name = "TongTien")
     private Float tongTien;
 
-
+    @Column(name= "NgayNhan")
+    private LocalDate ngayNhan;
 }
 
