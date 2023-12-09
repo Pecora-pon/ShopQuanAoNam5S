27e4a907afe54207d4cf7276a86e1a7b663ecd52
longthl <117787173+longthl@users.noreply.github.com>
2023-12-09 16:04:48 +0700

    Add files via upload

--- src/main/java/com/example/demo/service/DanhSachYeuThichService.java
+++ src/main/java/com/example/demo/service/DanhSachYeuThichService.java
@@ -1,13 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.DanhSachSanPhamYeuThich;
 import com.example.demo.entity.DanhSachYeuThich;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface DanhSachYeuThichService {
 
     List<DanhSachYeuThich> getAll();
+    List<DanhSachSanPhamYeuThich> getAlll();
 
     DanhSachYeuThich detail(Integer DanhSachYTID);
     DanhSachYeuThich update(Integer DanhSachYTID, DanhSachYeuThich danhSachYeuThich);
@@ -19,4 +22,8 @@ public interface DanhSachYeuThichService {
     void delete(Integer DanhSachYTID);
 
     List<DanhSachYeuThich> findKhachHang(String keyword);
+
+    DanhSachYeuThich them(DanhSachYeuThich danhSachYeuThich, UUID sanPhamID);
+
+    List<DanhSachSanPhamYeuThich> findKhachHang_name(String name);
 }
