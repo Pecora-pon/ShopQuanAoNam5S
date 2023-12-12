f1731829c531e6907b49659f82ddfebc570e8444
longthl <117787173+longthl@users.noreply.github.com>
2023-12-12 23:45:11 +0700

    Add files via upload

--- src/main/java/com/example/demo/repository/DanhSachSanPhamYeuThichRepo.java
+++ src/main/java/com/example/demo/repository/DanhSachSanPhamYeuThichRepo.java
@@ -1,18 +1,17 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.DanhSachSanPhamYeuThich;
-import com.example.demo.entity.GiamGia;
-import com.example.demo.entity.GioHang;
-import com.example.demo.entity.MauSac;
+import com.example.demo.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface DanhSachSanPhamYeuThichRepo extends JpaRepository<DanhSachSanPhamYeuThich,Integer> {
 //    @Query("SELECT dsspyt FROM DanhSachSanPhamYeuThich dsspyt WHERE dsspyt.sanPham LIKE %?1%")
 //    List<GiamGia> searchBydanhsachsanphamyeuthicid(int keyword);
+boolean existsBySanPham_SanPhamID(UUID sp);
 List<DanhSachSanPhamYeuThich>findByDanhSachYeuThich_KhachHang_Username(String username);
 }
