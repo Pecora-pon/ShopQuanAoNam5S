fd0a9768b95f52bdd6b1026f96afff9ae520c4f6
Perora <118367807+Pecora-pon@users.noreply.github.com>
2023-12-13 22:35:50 +0700

    Add files via upload

--- src/main/java/com/example/demo/config/Config.java
+++ src/main/java/com/example/demo/config/Config.java
@@ -26,6 +26,7 @@ import javax.crypto.spec.SecretKeySpec;
 public class Config {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_ReturnUrl = "http://localhost:8080/vnpay-payment";
+    public static String vnp_ReturnUrl1 = "http://localhost:8080/vnpay-payment1";
     public static String vnp_TmnCode = "LUB1A7JD";
     public static String secretKey = "MXBXQYUGHZOQVUYQLQWLSUXGSARIGBQW";
     public static String vnp_HashSecret = "";
