acc92286f95406c800ec97c60e6851b479734388
longthl <117787173+longthl@users.noreply.github.com>
2023-12-16 22:55:47 +0700

    Add files via upload

--- src/main/java/com/example/demo/repository/ThongTinVanChuyenRepo.java
+++ src/main/java/com/example/demo/repository/ThongTinVanChuyenRepo.java
@@ -2,9 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.GioHang;
 import com.example.demo.entity.NhanVien;
+import com.example.demo.entity.Size;
 import com.example.demo.entity.ThongTinVanChuyen;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +20,10 @@ public interface ThongTinVanChuyenRepo extends JpaRepository<ThongTinVanChuyen,I
     List<ThongTinVanChuyen> searchByDiaChi(String keyword);
     Optional<ThongTinVanChuyen> findById(Integer integer);
     List<ThongTinVanChuyen>findByKhachHang_Username(String username);
+    @Transactional
+    @Modifying
+    @Query(value = "Update ThongTinVanChuyen set trangThai = 1 where thongTinVanChuyenID =:thongTinVanChuyenID",nativeQuery = true)
+    void deleteByI(@Param("thongTinVanChuyenID") int chatLieuID);
+    @Query(value = "select * from ThongTinVanChuyen p WHERE p.trangThai =0 order by p.thongTinVanChuyenID desc",nativeQuery = true)
+    List<ThongTinVanChuyen> getAll();
 }
