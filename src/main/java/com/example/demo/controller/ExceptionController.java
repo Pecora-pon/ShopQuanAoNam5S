81527e460ae896e8d8a8dec7dae820333ed25b2b
Perora <118367807+Pecora-pon@users.noreply.github.com>
2023-12-17 21:11:18 +0700

    Add files via upload

--- src/main/java/com/example/demo/controller/ExceptionController.java
+++ src/main/java/com/example/demo/controller/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.Model;
@@ -26,5 +27,12 @@ public class ExceptionController {
     public String handleAccessDeniedException(AccessDeniedException e, Model model) {
         model.addAttribute("error", e.getMessage());
         return "error/403";
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public String handleDataIntegrityViolation(DataIntegrityViolationException e, Model model) {
+        model.addAttribute("error",  e.getMessage());
+        // Add any additional information to the model that you want to display on the error page.
+        return "error/402"; // Replace "error" with the name of your error view/template.
     }
 }
