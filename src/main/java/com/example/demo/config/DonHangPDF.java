eba7ab143ace4cb482e3ea9964edfb3ec4388f8f
CongHieu21113 <149700073+CongHieu21113@users.noreply.github.com>
2023-12-19 11:56:03 +0700

    Add files via upload

--- src/main/java/com/example/demo/config/DonHangPDF.java
+++ src/main/java/com/example/demo/config/DonHangPDF.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import com.example.demo.entity.DonHang;
+import com.example.demo.entity.DonHangChiTiet;
 import com.example.demo.entity.NhanVien;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
@@ -16,14 +17,14 @@ import java.util.List;
 public class DonHangPDF {
     private XSSFWorkbook workbook;
     private XSSFSheet sheet;
-    private DonHang donHang;
+    private DonHangChiTiet donHangChiTiet;
 
     public DonHangPDF() {
 
         workbook= new XSSFWorkbook();
     }
 
-    public byte[] exportpdf(DonHang donHang) throws IOException, DocumentException {
+    public byte[] exportpdf(DonHangChiTiet donHangChiTiet) throws IOException, DocumentException {
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             Document document = new Document();
             PdfWriter.getInstance(document, outputStream);
@@ -32,13 +33,14 @@ public class DonHangPDF {
 
             // Điền dữ liệu
 
-                document.add(new Paragraph("Id: " + donHang.getDonHangID()));
-                document.add(new Paragraph("Ten khach hang: " + donHang.getKhachHang().getHoTen()));
-                document.add(new Paragraph("So dien thoai: " + donHang.getKhachHang().getSoDienThoai()));
-                document.add(new Paragraph("Dia chi: " + donHang.getKhachHang().getDiaChi()));
-                document.add(new Paragraph("Hinh thuc thanh toan: " + donHang.getHinhThucThanhToan()));
-                document.add(new Paragraph("Ngay thanh toan: " + donHang.getNgayDatHang().toString()));
-
+                document.add(new Paragraph("Id: " + donHangChiTiet.getDonHangChiTietID()));
+                document.add(new Paragraph("Id: " + donHangChiTiet.getDonHang().getKhachHang().getHoTen()));
+                document.add(new Paragraph("Ngày đặt hàng: " + donHangChiTiet.getDonHang().getNgayDatHang().toString()));
+                document.add(new Paragraph("So dien thoai: " + donHangChiTiet.getDonHang().getKhachHang().getSoDienThoai()));
+                document.add(new Paragraph("Dia chi: " + donHangChiTiet.getDonHang().getKhachHang().getDiaChi()));
+                document.add(new Paragraph("Ten san pham : " + donHangChiTiet.getSanPham().getTenSanPham()));
+            document.add(new Paragraph("Ten san pham : " + donHangChiTiet.getSanPham().getTenSanPham()));
+            document.add(new Paragraph("Ten san pham : " + donHangChiTiet.getSanPham().getTenSanPham()));
                 document.add(new Paragraph(""));
 
 
