2877d0084f5b87917412b337ff46dc9001c2e482
longthl <117787173+longthl@users.noreply.github.com>
2023-12-19 11:05:27 +0700

    Add files via upload

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public UserDetailsService userDetailsServicekh(){
                 .authorizeRequests(authorize -> authorize
                         .requestMatchers(new AntPathRequestMatcher("/nhan-vien")).hasAuthority("ROLE_ADMIN")
                         .requestMatchers(new AntPathRequestMatcher("/gio-hang")).hasAuthority("ROLE_USER")
+                        .requestMatchers(new AntPathRequestMatcher("/san-pham")).hasAuthority("ROLE_ADMIN")
                         .requestMatchers(new AntPathRequestMatcher("/danh-sach-yt/hien-thi")).hasAuthority("ROLE_USER")
                         .requestMatchers(new AntPathRequestMatcher("/hien-thi")).hasAuthority("ROLE_ADMIN")
                         .requestMatchers(new AntPathRequestMatcher("/dangxem")).hasAuthority("ROLE_USER")
@@ -60,6 +61,7 @@ public UserDetailsService userDetailsServicekh(){
                         .requestMatchers(new AntPathRequestMatcher("/thuong-hieu")).hasAuthority("ROLE_ADMIN")
                         .requestMatchers(new AntPathRequestMatcher("/giam-gia")).hasAuthority("ROLE_ADMIN")
                         .requestMatchers(new AntPathRequestMatcher("/thong-tin-van-chuyen")).hasAuthority("ROLE_USER")
+                        .requestMatchers(new AntPathRequestMatcher("/nha-cung-cap")).hasAuthority("ROLE_ADMIN")
                         .requestMatchers(new AntPathRequestMatcher("/giam-gia-chi-tiet")).hasAuthority("ROLE_ADMIN")
                 )
                 .formLogin().loginPage("/login")
