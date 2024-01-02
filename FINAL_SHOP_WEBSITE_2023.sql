e23369dd5b3409ce6b7ba0d355d3dc5ed1fc9d9f
CongHieu21113 <149700073+CongHieu21113@users.noreply.github.com>
2024-01-02 14:14:45 +0700

    Add files via upload

--- FINAL_SHOP_WEBSITE_2023.sql
+++ FINAL_SHOP_WEBSITE_2023.sql
@@ -2,29 +2,32 @@
 USE	FINAL_SHOP_WEBSITE_2023
 DROP DATABASE FINAL_SHOP_WEBSITE_2023
 --các bảng chính
+
 GO
-CREATE TABLE ChucVu(
-	ChucVuID INT IDENTITY(1,1) PRIMARY KEY,
-	TenChucVu NVARCHAR(50),
+--CREATE TABLE ChucVu(
+	--ChucVuID INT IDENTITY(1,1) PRIMARY KEY,
+	--TenChucVu NVARCHAR(50),
 
-)
-INSERT INTO ChucVu(TenChucVu) VALUES (N'Quản lý')
-INSERT INTO ChucVu(TenChucVu) VALUES (N'Khách hàng')
-INSERT INTO ChucVu(TenChucVu) VALUES (N'Nhân viên')
-Select * from ChucVu
+--)
+--INSERT INTO ChucVu(TenChucVu) VALUES (N'Quản lý')
+--INSERT INTO ChucVu(TenChucVu) VALUES (N'Khách hàng')
+--Select * from ChucVu
 
-Go
+--Go
+
 CREATE TABLE KhachHang(
 	KhachHangID INT IDENTITY(1,1)PRIMARY KEY,
 	TenDangNhap NVARCHAR(255),
 	Email NVARCHAR(255),
-	MatKhau NVARCHAR(50),
+	MatKhau NVARCHAR(MAX),
 	HoTen NVARCHAR(255),																			
 	DiaChi NVARCHAR(255),
 	SoDienThoai NVARCHAR(50),
-	--ChucVu NVARCHAR(255),-- Có thể tách ra làm 1 bảng riêng
-	ChucVu INT FOREIGN KEY REFERENCES ChucVu(ChucVuID)
-
+	ChucVu NVARCHAR(MAX),
+	Reset_password_token VARCHAR(30),
+	is_reset_password_token_used BIT,
+	TrangThai INT
+    
 )
 
 Go
@@ -32,34 +35,39 @@ CREATE TABLE NhanVien(
 	NhanVienID INT IDENTITY(1,1)PRIMARY KEY,
 	TenDangNhap NVARCHAR(255),
 	Email NVARCHAR(255),
-	MatKhau NVARCHAR(50),
+	MatKhau NVARCHAR(MAX),
 	HoTen NVARCHAR(255),
 	DiaChi NVARCHAR(255),
 	SoDienThoai NVARCHAR(50),
 	TrangThai BIT,
-	--ChucVu NVARCHAR(255),-- Có thể tách ra làm 1 bảng riêng
-	ChucVu INT FOREIGN KEY REFERENCES ChucVu(ChucVuID)
+	ChucVu NVARCHAR(MAX),
 
 )
 Go
 CREATE TABLE ChatLieu(
 	ChatLieuID INT IDENTITY(1,1) PRIMARY KEY,
-	TenChatLieu NVARCHAR(255)
+	TenChatLieu NVARCHAR(255),
+	trangThai int
 )
 Go
 CREATE TABLE ThuongHieu(
 	ThuongHieuID INT IDENTITY(1,1) PRIMARY KEY,
-	TenThuongHieu NVARCHAR(255)
+	TenThuongHieu NVARCHAR(255),
+	trangThai int
 )
 GO
 CREATE TABLE MauSac(
 	MauSacID INT IDENTITY(1,1) PRIMARY KEY,
-	TenMauSac NVARCHAR(255)
+	TenMauSac NVARCHAR(255),
+	trangThai int,
+	SoLuong INT
 )
 GO
 CREATE TABLE Size(
 	SizeID INT IDENTITY(1,1) PRIMARY KEY,
 	TenSize NVARCHAR(255),
+	trangThai int,
+	SoLuong INT
 )
 Go
 CREATE TABLE SanPham(
@@ -74,12 +82,7 @@ CREATE TABLE SanPham(
 	SizeID INT FOREIGN KEY REFERENCES Size(SizeID),
 	ChatLieuID INT FOREIGN KEY REFERENCES ChatLieu(ChatLieuID),
 	ThuongHieuID INT FOREIGN KEY REFERENCES ThuongHieu(ThuongHieuID),
-)
-Go
-CREATE TABLE HinhAnh(
-	HinhAnhID INT IDENTITY(1,1) PRIMARY KEY,
-	HinhAnhURL NVARCHAR(MAX),
-	SanPhamID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES SanPham(SanPhamID)
+	HinhAnhURL NVARCHAR(Max),
 )
 Go
 CREATE TABLE GioHang(
@@ -87,6 +90,8 @@ CREATE TABLE GioHang(
 	KhachHangID INT FOREIGN KEY REFERENCES KhachHang(KhachHangID),
 	SanPhamID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES SanPham(SanPhamID),
 	SoLuongDat INT,
+	TongTien FLOAT,
+	trangThai INT
 )
 GO
 CREATE TABLE ThongTinVanChuyen(
@@ -94,15 +99,21 @@ CREATE TABLE ThongTinVanChuyen(
 	DiaChi NVARCHAR(MAX), -- Địa chỉ giao hàng,
 	PhuongThuc NVARCHAR(255), --Giao Hàng nhanh , giao hàng tiêu chuẩn
 	TrangThai INT , --0 là đang giao, 1 là đã giao xong
+	KhachHangID INT FOREIGN KEY REFERENCES KhachHang(KhachHangID)
+
 )
 GO
 CREATE TABLE GiamGia(
 	GiamGiaID INT IDENTITY(1,1) PRIMARY KEY,
 	MaGiamGia NVARCHAR(255),
-	SoTienGiam MONEY,
+	TenSuKien NVARCHAR(255),
+	NhanVienID INT FOREIGN KEY REFERENCES NhanVien(NhanVienID),
 	NgayTao DATE,
 	NgayHetHan DATE,
+	SoTienGiam float
 )
+Go
+
 GO
 CREATE TABLE DonHang(
 	DonHangID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
@@ -110,23 +121,69 @@ CREATE TABLE DonHang(
 	TenKhach NVARCHAR(255) NULL, --Null cho những khách hàng có tài khoản user
 	SoDienThoai INT Null, --Null cho những khách hàng có tài khoản user
 	ThongTinVanChuyenID INT FOREIGN KEY REFERENCES ThongTinVanChuyen(ThongTinVanChuyenID),
-	GiamGiaID INT FOREIGN KEY REFERENCES GiamGia(GiamGiaID),--Giảm giá theo từng sản phẩm
 	TrangThai INT, -- 0 là đang xử lý /1 là đã giao hàng
 	HinhThucThanhToan NVARCHAR(255), -- Thanh toán khi nhận hàng / thanh toán qua thẻ tín dụng/Momo. Có thể tách ra thành 1 table riêng
 	NgayDatHang DATE,
-	TongTien MONEY,
 	Note NVARCHAR(255), -- yêu cầu thêm của khách hàng
+	GiamGiaID INT FOREIGN KEY REFERENCES GiamGia(GiamGiaID),--Giảm giá theo từng sản phẩm
 )
 Go
 CREATE TABLE DonHangChiTiet(
-	DonHangChiTietID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
+	DonHangChiTietID INT IDENTITY(1,1) PRIMARY KEY,
 	DonHangID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES DonHang(DonHangID),
 	SanPhamID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES SanPham(SanPhamID),
 	SoLuong INT,
-	TrangThai BIT,
+	TrangThai int,
 	LyDoHuy NVARCHAR(255),
+	TongTien MONEY,
+	NgayNhan DATE
 
 )
+GO
+CREATE TABLE KhoHang(
+	KhoHangID INT IDENTITY(1,1) PRIMARY KEY,
+	SanPhamID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES SanPham(SanPhamID),
+	MauSacID INT FOREIGN KEY REFERENCES MauSac(MauSacID),
+	SizeID INT FOREIGN KEY REFERENCES Size(SizeID),
+	ChatLieuID INT FOREIGN KEY REFERENCES ChatLieu(ChatLieuID),
+	SoLuongTon INT,
+	NgayCapNhat DATE,
+)
+GO
+CREATE TABLE NhaCungCap(
+	NhaCungCapID INT IDENTITY (1,1) PRIMARY KEY,
+	TenNhaCungCap NVARCHAR(255),
+	DiaChi NVARCHAR(MAX),
+	SoDienThoai INT,
+	Email NVARCHAR(255),
+	trangThai int,
+)
+Go
+CREATE TABLE NhapKho(
+	NhapKhoID INT IDENTITY(1,1) PRIMARY KEY,
+	SanPhamID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES SanPham(SanPhamID),
+	MauSacID INT FOREIGN KEY REFERENCES MauSac(MauSacID),
+	SizeID INT FOREIGN KEY REFERENCES Size(SizeID),
+	ChatLieuID INT FOREIGN KEY REFERENCES ChatLieu(ChatLieuID),
+	SoLuongNhap INT,
+	NgayNhap DATE,
+	NhaCungCapID INT FOREIGN KEY REFERENCES NhaCungCap(NhaCungCapID),
+	trangThai int,
+)
+GO
+CREATE TABLE XuatKho(
+	XuatKhoID INT IDENTITY(1,1) PRIMARY KEY,
+	SanPhamID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES SanPham(SanPhamID),
+	MauSacID INT FOREIGN KEY REFERENCES MauSac(MauSacID),
+	SizeID INT FOREIGN KEY REFERENCES Size(SizeID),
+	ChatLieuID INT FOREIGN KEY REFERENCES ChatLieu(ChatLieuID),
+	SoLuongXuat INT,
+	NgayXuat DATE,
+	NhaCungCapID INT FOREIGN KEY REFERENCES NhaCungCap(NhaCungCapID),
+	trangThai int,
+
+)
+
 --Các bảng khác
 GO
 CREATE TABLE DanhSachYeuThich(
@@ -144,7 +201,8 @@ CREATE TABLE ReviewNguoiDung(
 	 ReviewID INT IDENTITY(1,1) PRIMARY KEY,
 	 SanPhamID UNIQUEIDENTIFIER FOREIGN KEY REFERENCES SanPham(SanPhamID),
 	 KhachHangID INT FOREIGN KEY REFERENCES KhachHang(KhachHangID),
-	 BinhLuan NVARCHAR(MAX)
+	 BinhLuan NVARCHAR(MAX),
+	 HinhAnhURL NVARCHAR(Max),
 )
 
 
