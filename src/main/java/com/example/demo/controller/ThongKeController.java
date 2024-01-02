3af105a4e846c72ca1f4b3090715f89e5f46a327
Perora <118367807+Pecora-pon@users.noreply.github.com>
2024-01-02 15:47:56 +0700

    Add files via upload

--- src/main/java/com/example/demo/controller/ThongKeController.java
+++ src/main/java/com/example/demo/controller/ThongKeController.java
@@ -5,12 +5,15 @@ import com.example.demo.entity.NhanVien;
 import com.example.demo.entity.SanPham;
 import com.example.demo.service.ThongKeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 @Controller
 public class ThongKeController {
@@ -18,7 +21,12 @@ public class ThongKeController {
     private ThongKeService thongKeService;
 
     @GetMapping("/thongke")
-    public String ThongKe(Model model) {
+    public String ThongKe(Model model, @RequestParam(name = "selectedDate", required = false) LocalDate selectedDate) {
+        if (selectedDate == null) {
+            selectedDate = LocalDate.now();
+            return "redirect:/thongke?selectedDate=" + selectedDate;
+        }
+
         int trangThai = 3;
 
         List<NhanVien> nhanViens = thongKeService.getAllNhanVien();
@@ -43,8 +51,14 @@ public class ThongKeController {
         Float totalTongTien = thongKeService.calculateTotalTongTien(trangThai);
         model.addAttribute("totalTongTien",totalTongTien);
 
-        List<Object[]> totalQuantityByMonth = thongKeService.getTotalQuantityByMonthInYear2023();
+        List<Object[]> totalQuantityByMonth = thongKeService.getTotalQuantityByMonthInYear2023(selectedDate);
         model.addAttribute("totalQuantityByMonth", totalQuantityByMonth);
+
+        List<Object[]> getTotalRevenueForCurrentMonth = thongKeService.getTotalRevenueForCurrentMonth();
+        model.addAttribute("getTotalRevenueForCurrentMonth",getTotalRevenueForCurrentMonth);
+
+        Long totalQuantity = thongKeService.gettotalQuantity();
+        model.addAttribute("totalQuantity",totalQuantity);
 
 
         long countOrdersByStatus = thongKeService.countOrdersByStatus();
@@ -62,6 +76,7 @@ public class ThongKeController {
 
         List<Object[]> totalRevenueByMonth = thongKeService.getTotalRevenueByMonthInYear2023();
         model.addAttribute("totalRevenueByMonth", totalRevenueByMonth);
+        model.addAttribute("selectedDate", selectedDate);
 
             List<Object[]> topProducts = thongKeService.getTopProducts();
             model.addAttribute("topProducts",topProducts);
