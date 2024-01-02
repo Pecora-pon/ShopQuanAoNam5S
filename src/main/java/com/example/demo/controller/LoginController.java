9cf55b6e05379424ccce1cdbc4f5badd3f179c6c
Perora <118367807+Pecora-pon@users.noreply.github.com>
2024-01-02 15:48:50 +0700

    Add files via upload

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,13 +1,27 @@
 
 package com.example.demo.controller;
 
+import com.example.demo.repository.DonHangChiTietRepo;
+import com.example.demo.service.ThongKeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
 public class LoginController {
-
+    @Autowired
+    private DonHangChiTietRepo donHangChiTietRepo;
+    @GetMapping("/your-api-endpoint")
+    public List<Object[]> getDataForSelectedDate(@RequestParam("selectedDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate selectedDate) {
+        return donHangChiTietRepo.getTotalQuantityByMonthInYear2023(selectedDate);
+    }
 }
 
