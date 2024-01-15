0a34f187f399cdb91e9f04c8ba9773ff475d91ad
longthl <117787173+longthl@users.noreply.github.com>
2024-01-15 21:32:49 +0700

    Add files via upload

--- src/main/java/com/example/demo/service/BanHangService.java
+++ src/main/java/com/example/demo/service/BanHangService.java
@@ -3,20 +3,33 @@ package com.example.demo.service;
 import com.example.demo.entity.DonHang;
 import com.example.demo.entity.DonHangChiTiet;
 import com.example.demo.entity.SanPham;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface BanHangService {
-    DonHangChiTiet insert(DonHang donHang,DonHangChiTiet donHangChiTiet);
+    DonHangChiTiet insert(DonHang donHang, DonHangChiTiet donHangChiTiet);
 
     void insertdh(DonHang donHang);
-    List<DonHangChiTiet>getAll(UUID id);
-    DonHangChiTiet insertsp(UUID sanPham, UUID donHang,int soLuong);
+
+    List<DonHangChiTiet> getAll(UUID id);
+
+    DonHangChiTiet insertsp(UUID sanPham, UUID donHang, int soLuong);
+
     SanPham detail(UUID id);
-    List<DonHang>getAlll();
-    List<DonHangChiTiet> banhang(List<Integer> donhang,float tong);
+
+    List<DonHang> getAlll();
+
+    List<DonHangChiTiet> banhang(List<Integer> donhang, float tong,int khachhang);
+
+    List<DonHangChiTiet> banhang1(List<Integer> donhang, float tong);
+
     void delete(int id);
+
     void delete1(UUID id);
+
     List<SanPham> findbyten(String ten);
+
+    Page<SanPham> getPage(int pageNumber, int pageSize);
 }
